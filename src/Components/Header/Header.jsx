17e23cc3bb350a06a9e2dba8c8857ed0edef097d
Fasiku Daniel <daniel.fasiku@stu.cu.edu.ng>
2024-05-14 17:20:00 +0100

    Tool tips style changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -21,24 +21,30 @@ function Header() {
         <input className='search-bar' type="text" placeholder='Search' />
         <button className='search'>
           <img src={Search} alt="search" className='search-icon' />
+          <div className='tool-tips'>Search</div>
         </button>
         <button className='mic-button'>
           <img src={Mic} alt="mic" className='mic-icon' />
+          <div className='tool-tips'>Search with your voice</div>
         </button>
       </div>
       <div className='rhd-section'>
         <div className='icon-cont'>
           <img src={UploadVideo} alt="upload" className='icon1' />
+          <div className='tool-tips'>Create</div>
         </div>
         <div className='icon-cont'>
           <img src={Apps} alt="youtube-apps" className='icon2' />
+          <div className='tool-tips'>Apps</div>
         </div>
         <div className='not-cont'>
           <img src={Notifications} alt="notifications" className='icon3' />
           <div className='not-num'>3+</div>
+          <div className='tool-tips'>Notifications</div>
         </div>
-        <div>
+        <div className='icon-cont-2'>
           <img src={ProfilePic} alt="profile-pic" className='icon4' />
+          <div className='tool-tips'>Profile</div>
         </div>
       </div>
     </header>
